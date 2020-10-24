6070d9fb157a96b8c3e9a1f22e8bd2fe6ef319a5
Pavlos Saba <pavlossaba@gmail.com>
2020-10-24 11:28:43 +0300

    Simple functionality of Products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
+import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
 
 const App = () => {
   return (
-    <>
-      <h1>Welcome To roshop</h1>
-    </>
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/product/:id" component={ProductPage} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 };
 
